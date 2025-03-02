76650429aac4465c5b184534e63ef697639d98e7
Thiwanka836 <nayanajiththiwanka194@gmail.com>
2025-03-02 13:12:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KHAN-MD~WjYGEKRZ#VoP4L79AyLxcAgKWqcAOW06uMm1BrjWutoJk8MrYvBk",
+SESSION_ID: process.env.SESSION_ID || "KHAN-MD~P3IyEbpB#bKDLV2uuxRHjwtvt1uV1CaZ2buJmaanE379oGeH5sV8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "false",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*`ඔයාගෙ ස්ටෙටස්  බැලුවා පැතියො ඉතිම් මගෙත් බලන්න දැම්ම ගිහිම් 🙈💐`*",
 // set the auto reply massage on status reply  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -23,7 +23,7 @@ STICKER_NAME: process.env.STICKER_NAME || "KHAN-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💐,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
